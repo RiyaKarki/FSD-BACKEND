8ddf8b1b150cf9961a7ac8de61b490c37c669a56
RiyaKarki <karkiriya0029@gmail.com>
2025-03-28 12:23:32 +0530

    design

--- Day5/frontend/src/App.jsx
+++ Day5/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Register from './components/Register'
+import View from './components/View'
 
 const App = () => {
   return (
     <div>
-      <h1>User Registrstion System</h1>
+      <h1 style={{backgroundColor: 'violet'}}>User Registrstion System</h1>
       <Register/>
+      <View/>
     </div>
   )
 }
